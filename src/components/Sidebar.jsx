12eb3483cc1c8964002042da1f7935ffed33b715
Esteban Martinez <Arepo90@proton.me>
2022-10-16 18:54:33 -0500

    v0.2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   const handleCloseSidebar = () => {
-    if(activeMenu || screenSize <= 900){
+    if(activeMenu){
       setActiveMenu(false);
     }
   }
@@ -26,8 +26,8 @@ const Sidebar = () => {
           <Link to="/" onClick={handleCloseSidebar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <img src={logo} className="w-10" /><span>Rotas</span>
           </Link>
-          <TooltipComponent content="Menu" position="BottomCenter">
-            <button type="button" onClick={() => setActiveMenu(false)} style={{ color: 'rgb(50, 50, 50)', borderRadius: '50%' }} className="text-2xl p-3 mt-4 hover:drop-shadow-xl hover:bg-light-gray">
+          <TooltipComponent content="Close" position="BottomCenter">
+            <button type="button" onClick={() => setActiveMenu(false)} style={{ color: 'rgb(153, 171, 180)', borderRadius: '50%' }} className="text-2xl p-3 mt-4 hover:drop-shadow-xl hover:bg-light-gray">
               <MdOutlineCancel />
             </button>
           </TooltipComponent>
--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useStateContext } from '../contexts/ContextProvider';
 import { Header } from '../components';
+import { useState } from 'react';
 
 //El propio programa si funciona, nada mas hay que recoger los datos
 
-let m, g, k, L, hf, h0, h, t, vix, viy, vi, a, x;
+let m = 0.5, g = -9.81, k = 450, L = 20, hf = 10, h0 = 0, h, t, vix, viy, vi, a, x;
 let d, f, G, H, R, S, T, U, x1, x2;
 
-function safetyChech(){
+function safetyCheck(){
   let check = (Math.pow(vi, 2.0) / (2*g)) - ((g*Math.pow(L, 2.0)) / (2*Math.pow(vi, 2.0)));
   if(check > h){
     return false;
@@ -40,11 +41,26 @@ function solve1(){
 }
 
 const Calculator = () => {
+  const { currentColor } = useStateContext();
+  const [masa, setMasa] = useState('');
+
+  const submissionHandler = (e) => {
+    e.preventDefault();
+    console.log(masa);
+  };
+
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 p-2 md:p-10 dark:bg-slate-800 bg-white rounded-xl">
       <Header title="Calculator" />
-      <button type="button" onClick={solve1}>botonlol</button>
-      <p>elpepe {a}</p>
+      <form onSubmit={submissionHandler}>
+      <div class="relative z-0">
+        <input type="text" id="masa" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " onChange={(e) => {setMasa(e.target.value)}} style={{borderColor: currentColor}}/>
+          <label for="floating_standard" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Masa</label>
+        </div>
+        <div className="mt-10">
+          <button type="submit" className="p-2 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{ background: currentColor, borderRadius: '10%' }}>Submit</button>
+        </div>
+      </form>
     </div>
   )
 }
